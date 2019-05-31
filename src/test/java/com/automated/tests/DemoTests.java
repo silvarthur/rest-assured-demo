0aa4c06ef1d321129071ad6c0aea20762c2f6839
Arthur Antunes <arthuragds@gmail.com>
2019-05-30 21:31:29 -0300

    improving the test case

--- src/test/java/com/automated/tests/DemoTests.java
+++ src/test/java/com/automated/tests/DemoTests.java
@@ -2,6 +2,8 @@ package com.automated.tests;
 
 import org.junit.Test;
 
+import io.restassured.http.ContentType;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -10,11 +12,18 @@ public class DemoTests {
 	@Test
 	public void test() {
 		given().
+			log().all().
 		when().
 			get("http://api.zippopotam.us/us/90210").
 		then().
 			assertThat().
-			body("places[0].state", equalTo("California"));
+			contentType(ContentType.JSON).
+			and().
+			statusCode(200).
+			and().
+			body("places[0].state", equalTo("California")).
+			and().
+			log().body();
 	}
 
 }
